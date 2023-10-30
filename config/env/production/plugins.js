17c1342fa2b7378074107557cdfec345bef5d3f7
Julian Viso <julian.viso.romero@gmail.com>
2023-10-29 22:51:13 -0300

    dele algolia

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -14,15 +14,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  search: {
-    enabled: true,
-    config: {
-      provider: 'algolia',
-      providerOptions: {
-        apiKey: env('ALGOLIA_PROVIDER_ADMIN_API_KEY'),
-        applicationId: env('ALGOLIA_PROVIDER_APPLICATION_ID'),
-      },
-      contentTypes: [{ name: 'api::post.post' }],
-    },
-  },
 });
